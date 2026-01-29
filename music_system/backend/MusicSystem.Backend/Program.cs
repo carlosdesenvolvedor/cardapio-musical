8f39d1a63c5c7a9f49dd331fe6a24eb99e1cd47d
carlos cezar nunes de souza <carlosjsciaa@gmail.com>
2026-01-29 14:35:45 -0300

    Fix: Removed OpenApi calls from Program.cs to fix source build

--- music_system/backend/MusicSystem.Backend/Program.cs
+++ music_system/backend/MusicSystem.Backend/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
 builder.Services.AddControllers();
 
 // Register Storage Service
@@ -30,10 +30,7 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+
 
 app.MapControllers();
 
